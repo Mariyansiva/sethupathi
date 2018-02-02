bb0dfa7349c0e01bdf59736b3e79b7c4acc4730d
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-02-02 14:12:56 +0530

    Create siva1.c

---
+++ siva1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("the value is:");
+ scanf("%d",&a);
+if(a>0)
+{
+    printf("value is possitive");
+}
+else if(a<0)
+{
+    printf(" value is nagative");
+}
+else if(a==0)
+ {
+     printf("value is zero");
+ }
+     return 0;
+}
+
