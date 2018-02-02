9762bc8a307b2e14dbffdff19fc47a09598812c2
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-02-02 14:17:26 +0530

    Create siva2.c

---
+++ siva2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("the value is:");
+ scanf("%d",&a);
+b=a%2;
+if(b==0)
+{
+    printf("value is even");
+}
+else if(b!=0)
+{
+    printf(" value is odd");
+}
+     return 0;
+}
+
+
